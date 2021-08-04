7027cae7d9bdc9d772c68cca7bcba487009cdfee
Javier <test@mail.com>
2021-08-04 18:50:28 -0300

    feat: adding decimals to prices

--- api/index.php
+++ api/index.php
@@ -6,41 +6,41 @@
     "nombre": "Rainbow Six",
     "imagen": "RainbowSix.jpg",
     "descripcion": "Tom Clancys Rainbow Six Siege es el nuevo lanzamiento del shooter más aclamado desarrollado por el estudio Ubisoft Montreal.",
-    "precio": 9030
+    "precio": 9030.23
   },
   {
     "id": 2,
     "nombre": "PUBG",
     "imagen": "PUBG.jpg",
     "descripcion": "PLAYERUNKNOWN’S BATTLEGROUNDS es un shooter basado en el modo Battle Royale que está siendo desarrollado a través de la retroalimentación con la comunidad. Comenzando de la nada, los usuarios tienen que luchar uno contra el otro para localizar armas y suministros para ser el único ganador.",
-    "precio": 1150
+    "precio": 1150.11
   },
   {
     "id": 3,
     "nombre": "Terraria",
     "imagen": "Terraria.jpg",
     "descripcion": "¡Cava, lucha, explora, construye! Con este juego de aventuras repleto de acción nada es imposible. ¡Pack de Cuatro también disponible!",
-    "precio": 100
+    "precio": 100.89
   },
   {
     "id": 4,
     "nombre": "Raft",
     "imagen": "Raft.jpg",
     "descripcion": "¡Raft te lanza a ti y a tus amigos a una aventura oceánica épica! ¡Solo o juntos, los jugadores luchan por sobrevivir a un peligroso viaje a través del mar! Recolecta escombros, busca arrecifes y construye tu propia casa flotante, ¡pero ten cuidado con los tiburones devoradores de hombres!",
-    "precio": 400
+    "precio": 400.33
   },
   {
     "id": 5,
     "nombre": "Phasmo",
     "imagen": "Phasmophobia.jpg",
     "descripcion": "Phasmophobia es un juego de terror psicológico cooperativo en línea para 4 jugadores. La actividad paranormal va en aumento y depende de ti y de tu equipo utilizar todo el equipo de caza de fantasmas a tu disposición para reunir la mayor cantidad de evidencia posible.",
-    "precio": 415
+    "precio": 415.23
   },
   {
     "id": 6,
     "nombre": "Fall Guys",
     "imagen": "FallGuys.jpg",
     "descripcion": "Fall Guys es un juego multijugador masivo tipo party con hasta 60 jugadores online en un enfrentamiento todos contra todos que se desarrolla ronda tras ronda entre un caos creciente hasta que solo queda un único vencedor.",
-    "precio": 750
+    "precio": 750.15
   }
 ]
